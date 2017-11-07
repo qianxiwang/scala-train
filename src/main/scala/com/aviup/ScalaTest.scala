8579f523d943cc7f8f494af1df8eb3043cc56369
010 <153390619@qq.com>
2017-11-07 13:54:13 +0800

    添加注释

--- src/main/scala/com/aviup/ScalaTest.scala
+++ src/main/scala/com/aviup/ScalaTest.scala
@@ -25,19 +25,27 @@ object ScalaTest {
     }
 
   def changeFileName(fileSystem: FileSystem, outputPath: String, loadTime: String, partition: String): Unit = {
+    //返回一个Path序列（hdfs://192.168.31.89:8020/spark/emp/temp/201711112025/d=20171111/h=20/*.txt）
     val paths = SparkHadoopUtil.get.globPath(new Path(outputPath + "temp/" + loadTime + partition + "/*.txt"))
     var times = 0
     paths.map(x => {
+      //将hdfs://192.168.31.89:8020/spark/emp/temp/201711112025替换成hdfs://192.168.31.89:8020/spark/emp/data/
+      //hdfs://192.168.31.89:8020/spark/emp/data/d=20171111/h=20/part-r-00002-6ba69620-ba52-4cb1-9ea0-6634ae0e16bc.txt
       var newLocation = x.toString.replace(outputPath + "temp/" + loadTime, outputPath + "data/")
       println("1:"+newLocation)
+      //hdfs://192.168.31.89:8020/spark/emp/data/d=20171111/h=20/00002-6ba69620-ba52-4cb1-9ea0-6634ae0e16bc.txt
       newLocation = newLocation.replace("part-r-", "")
+      //获取最后一个"/"的索引
       val index = newLocation.lastIndexOf("/")
       times += 1
+      //hdfs://192.168.31.89:8020/spark/emp/data/d=20171111/h=20/201711112025-times.txt
       newLocation = newLocation.substring(0, index + 1) + loadTime + "-" + times + ".txt"
       println("2:"+newLocation)
       val officialPath = new Path(newLocation)
-      if (!fileSystem.exists(officialPath.getParent))
+      //如果hdfs://192.168.31.89:8020/spark/emp/data/d=20171111/h=20/不存在，就创建它
+      if (!fileSystem.exists(officialPath.getParent)) {
         fileSystem.mkdirs(officialPath.getParent)
+      }
       fileSystem.rename(x, officialPath)
     })
   }
